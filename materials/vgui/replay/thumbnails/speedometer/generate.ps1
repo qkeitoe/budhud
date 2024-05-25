c42288395b65d23bcca7af9fc97b2763ce973e69
qkeitoe <qkeitoe@yandex.com>
2024-05-25 10:18:18 +0300

    Make speedometer just white and increase its size

--- materials/vgui/replay/thumbnails/speedometer/generate.ps1
+++ materials/vgui/replay/thumbnails/speedometer/generate.ps1
@@ -8,76 +8,10 @@
 # Define parameters
 $maxNumber = 3500
 $backgroundColor = "Transparent"
-$font = "LatoSemibold.ttf"
-$fontSize = 32
-$VTFSize = "128x32"
+$font = "font-latosemibold-original.ttf"
+$fontSize = 64
+$VTFSize = "256x64"
 $imagesFolder = "$PSScriptRoot\numbers"  # Assuming the images are stored in a folder named "numbers" in the script's directory
-
-# Function to calculate the text color based on the number
-function Get-TextColor {
-    param(
-        [int]$number
-    )
-
-    if ($number -ge 0 -and $number -le 199) {
-        # Calculate gradient color from gray to white
-        $startColor = @(128, 128, 128)  # Gray
-        $endColor = @(255, 255, 255)    # White
-
-        $numSteps = 199
-        $percent = $number / $numSteps
-
-        $r = $startColor[0] + [int]($percent * ($endColor[0] - $startColor[0]))
-        $g = $startColor[1] + [int]($percent * ($endColor[1] - $startColor[1]))
-        $b = $startColor[2] + [int]($percent * ($endColor[2] - $startColor[2]))
-
-        return "rgb($r,$g,$b)"
-    }
-    elseif ($number -ge 200 -and $number -le 400) { # Scout run speed is 400
-        return "rgb(255,255,255)"  # White
-    }
-    elseif ($number -ge 401 -and $number -le 499) {
-        # Calculate gradient color from white to green
-        $hue = 120  # Green color in HSL color space
-        $saturation = 100
-        $lightness = 100 - (($number - 350) / (499 - 350) * 50)  # Brightness decreases from 100 to 50
-        return "hsl($hue,$saturation%,$lightness%)"
-    }
-    elseif ($number -ge 500 -and $number -le 1499) {
-        # Calculate gradient color from green to blue
-        $startColor = @(0, 255, 0)  # Green
-        $endColor = @(4, 217, 255)  # Blue
-
-        $numSteps = 1499 - 501
-        $percent = ($number - 501) / $numSteps
-
-        $r = $startColor[0] + [int]($percent * ($endColor[0] - $startColor[0]))
-        $g = $startColor[1] + [int]($percent * ($endColor[1] - $startColor[1]))
-        $b = $startColor[2] + [int]($percent * ($endColor[2] - $startColor[2]))
-
-        return "rgb($r,$g,$b)"
-    }
-    elseif ($number -ge 1500 -and $number -le 3499) {
-        # Calculate gradient color from blue to purple
-        $startColor = @(4, 217, 255)  # Blue
-        $endColor = @(188, 19, 254)    # Purple
-
-        $numSteps = 3499 - 1500
-        $percent = ($number - 1500) / $numSteps
-
-        $r = $startColor[0] + [int]($percent * ($endColor[0] - $startColor[0]))
-        $g = $startColor[1] + [int]($percent * ($endColor[1] - $startColor[1]))
-        $b = $startColor[2] + [int]($percent * ($endColor[2] - $startColor[2]))
-
-        return "rgb($r,$g,$b)"
-    }
-    elseif ($number -eq 3500) {
-        return "rgb(255,49,49)"   # Bright red
-    }
-    else {
-        return "rgb(0,0,0)"   # Black (fallback color)
-    }
-}
 
 # Function to display progress bar
 function Show-ProgressBar {
@@ -100,7 +34,7 @@ Write-Host "Starting script..."
 
 for ($num = 0; $num -le $maxNumber; $num++) {
     # Calculate text color based on the number
-    $textColor = Get-TextColor -number $num
+    $textColor = "rgb(255,255,255)"
     # Display progress
     Write-Host -NoNewline "`rGenerating number $num with text color: $textColor"
     # Generate image
@@ -111,16 +45,6 @@ for ($num = 0; $num -le $maxNumber; $num++) {
 }
 
 Write-Host "Script completed."
-
-# Regenerate images around 1100 (jump rampsync)
-for ($num = 1105; $num -le 1115; $num++) {
-    # Generate image with specific color
-    $textColor = "rgb(255,215,0)"  # Yellowish color
-    ./convert.exe -auto-orient -background $backgroundColor -fill "$textColor" -font $font -size $VTFSize -gravity center -pointsize $fontSize -antialias label:$num "$imagesFolder\$num.png"
-}
-
-# Write a message indicating that imageshave been regenerated
-Write-Host "Rampsync images regenerated."
 
 # Copy over specific images to prevent max run speed bouncing between values
 $numbersToCopy = @(
